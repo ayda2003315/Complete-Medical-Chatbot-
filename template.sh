073b1a334ebcf87e07f70dc0a593ead650084e04
Aydouda <aydabenabdelhafidh@gmail.com>
2025-12-04 09:38:31 +0100

    folder structure added

---
+++ template.sh
@@ -0,0 +1,18 @@
+# creating directory
+mkdir -p src
+mkdir -p research
+
+
+# creating file
+touch src/__init__.py 
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch src/app.py
+touch research/tials.ipynb
+touch requirements.txt
+
+
+
+echo "directory and files are creates successfully "
